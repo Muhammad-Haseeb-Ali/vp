b8531edc204dbb67f742ed935d4493b842eb26a3
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-11-01 20:54:15 +0500

    corrects if conditions in checkexistence.js

--- pages/api/checkexistence.js
+++ pages/api/checkexistence.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 export default function handler(req , res){
     const id = req.query.id;
-if (fs.existsSync(`./public/videos/${id}/screen.mp4`) && fs.existsSync(`./public/videos/${id}/face.mp4`)) 
+if (fs.existsSync(`./public/videos/${id}.zip`) && fs.existsSync(`./public/videos/${id}`)) 
 {
     res.status(200).send("Content exist")
 }
