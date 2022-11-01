49bbf82e63ab8c4e430d7e420383044bbf9af983
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-11-01 20:47:47 +0500

    adds redirect on index url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source:'/',
+        destination: '/proposal?id=786',
+        permanent: true,
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
