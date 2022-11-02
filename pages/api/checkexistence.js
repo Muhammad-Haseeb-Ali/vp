54a8df12c5a5597b6ec2e8bf1b19007ece277c72
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-11-02 17:48:51 +0500

    hard code

--- pages/api/checkexistence.js
+++ pages/api/checkexistence.js
@@ -1,7 +1,8 @@
+const path = require('path')
 const fs = require('fs')
 export default function handler(req , res){
     const id = req.query.id;
-if (fs.existsSync('./public/videos/face/0000.zip')) 
+if (fs.existsSync(path.resolve('./public/videos/face', '0000.zip'))) 
 {
     res.status(200).send("Content exist")
 }
