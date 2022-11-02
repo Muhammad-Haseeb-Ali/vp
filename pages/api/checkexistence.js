2ef06f3f53036cec3d599123b3f1724021b9a122
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-11-02 16:34:18 +0500

    ...

--- pages/api/checkexistence.js
+++ pages/api/checkexistence.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 export default function handler(req , res){
     const id = req.query.id;
-if ((fs.existsSync(`./public/videos/screen/${id}.mp4`) && fs.existsSync(`./public/videos/face/${id}.mp4`)) || (fs.existsSync(`./public/videos/screen/${id}.zip`) && fs.existsSync(`./public/videos/face/${id}.zip`))) 
+if ((fs.existsSync(`/public/videos/screen/${id}.mp4`) && fs.existsSync(`/public/videos/face/${id}.mp4`)) || (fs.existsSync(`./public/videos/screen/${id}.zip`) && fs.existsSync(`./public/videos/face/${id}.zip`))) 
 {
     res.status(200).send("Content exist")
 }
