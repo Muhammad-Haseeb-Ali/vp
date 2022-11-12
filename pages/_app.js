96d65bd3c790ac0c8b8b837e50a2a6bf91906605
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-11-12 15:18:28 +0500

    another

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+      <Head>
+    <title>VP by Muhammad H.</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+  </Head>
+   <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
 import styles from '../styles/Home.module.css'
-import Head from 'next/head'
 
 export default function Home() {
 
-  return (
-    <>
-    <Head>
-    <title>VP by Muhammad H.</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-  </Head>
-    <h1>Hello World</h1>
-    </>
-  )
+  return null
 }
