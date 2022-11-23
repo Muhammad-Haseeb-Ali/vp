6f67c201ea9b879a0bcc32766dc969c9c8aa56ca
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-11-23 21:50:22 +0500

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
     <title>VP by Muhammad H.</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
   </Head>
    <Component {...pageProps} />
   </>)
