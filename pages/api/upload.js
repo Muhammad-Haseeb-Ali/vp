9193f54ff5b00a1157a5a695d5e6c6b97ff95ccd
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-12-05 21:37:11 +0500

    ...

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -52,6 +52,25 @@ export default function handler(req, res){
     return res.status(403).json({status: false , discription: "This method is not allowed." })
   }
 
-  post(req, res)
+  const form = new formidable.IncomingForm();
+  form.parse(req,(err, fields, files) => {
+    if(err){
+      return res.status(404).json({status: false, error: "Error: Request is not parsed correctly"})
+    }
+    console.log("----------------------------",form._parser.globalOptions.maxFileSize)
+    const data = fs.readFileSync(files.file.filepath);
+    // comment for removing fs functionalities
+    // if (!fs.existsSync(resourcesDir)){
+    //   fs.mkdirSync(resourcesDir);
+    // }
+    // fs.writeFileSync(`${resourcesDir}/${files.file.originalFilename}`, data);
+    // if(!fs.existsSync(`${resourcesDir}/${files.file.originalFilename}`) && fs.existsSync(files.file.filepath))
+    // {
+    //   fs.unlinkSync(files.file.filepath);
+    //   console.log("temporery stored filr is deleted!")
+    //         return res.status(404).json({status: false, error: "error occure in file creation face"})
+    // }
+     res.status(200).json({status: true, path: files.file.filepath });
+  });
 
 };
