5fe402da63d460d6b4f19ec3a7350485f8152fcd
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-12-10 16:29:27 +0500

    another

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,10 +1,8 @@
-  export const config = {
-    api: {
-      bodyParser: false, // Defaults to true. Setting this to false disables body parsing and allows you to consume the request body as stream or raw-body.
-      responseLimit: false, // Determines how much data should be sent from the response body. It is automatically enabled and defaults to 4mb.
-      externalResolver: true, // Disables warnings for unresolved requests if the route is being handled by an external resolver like Express.js or Connect. Defaults to false.
-    },
-  }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 export default function handler(req, res){
   if(req.method !== 'POST')
