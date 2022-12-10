073ab3c53cbf5ce974ce88649a657a0dd414488f
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-12-10 16:13:51 +0500

    makes upload api simple

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,21 +1,3 @@
-import formidable from "formidable";
-import fs from "fs";
-import path from "path";
-
-const directoryPath = e => path.join(process.cwd(), e),
-      resourcesDir = directoryPath("/public/resources")
-
-//       uploadMiddleware = multer({
-//         storage: multer.diskStorage({
-//           destination: function (req, file, cb) {
-//             cb(null, "resources");
-//           },
-//           filename: function (req, file, cb) {
-//             cb(null, file.filename + ".zip");
-//           }
-//         })
-//       }).single("file");
-
   export const config = {
     api: {
       bodyParser: false, // Defaults to true. Setting this to false disables body parsing and allows you to consume the request body as stream or raw-body.
@@ -30,17 +12,6 @@ export default function handler(req, res){
     res.setHeader('Allow', 'POST');
     return res.status(403).json({status: false , discription: "This method is not allowed." })
   }
-
-  // const form = new formidable.IncomingForm({
-  //   maxFileSize: 1 * 1024 * 1024,
-  //   keepExtensions: true
-  // });
-  // form.parse(req,(err, fields, files) => {
-  //   if(err){
-  //     return res.status(404).json({status: false, error: "Error: Request is not parsed correctly"})
-  //   }
-  //    res.status(200).json({status: true});
-  // });
   res.status(200).json({status: true});
 
 };
