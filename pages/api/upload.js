98b1bcebb8a3f9a483064b1f12a646100c39c358
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-12-10 13:27:50 +0500

    adds formidable in upload api

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -22,15 +22,20 @@ export const config = {
   }
 };
 
-const post = (req, res) => {
+export default function handler(req, res){
   console.log(req)
+  if(req.method !== 'POST')
+  {
+    return res.status(403).json({status: false , discription: "This method is not allowed." })
+  }
+
   const form = new formidable.IncomingForm();
   form.parse(req,(err, fields, files) => {
     if(err){
       return res.status(404).json({status: false, error: "Error: Request is not parsed correctly"})
     }
-    console.log("----------------------------",form._parser.globalOptions.maxFileSize)
-    const data = fs.readFileSync(files.file.filepath);
+    // console.log("----------------------------",form._parser.globalOptions.maxFileSize)
+    // const data = fs.readFileSync(files.file.filepath);
     // comment for removing fs functionalities
     // if (!fs.existsSync(resourcesDir)){
     //   fs.mkdirSync(resourcesDir);
@@ -44,36 +49,5 @@ const post = (req, res) => {
     // }
      res.status(200).json({status: true, path: files.file.filepath });
   });
-};
-
-export default function handler(req, res){
-  console.log(req)
-  if(req.method !== 'POST')
-  {
-    return res.status(403).json({status: false , discription: "This method is not allowed." })
-  }
-
-  res.status(200).json({status: true });
-
-  // const form = new formidable.IncomingForm();
-  // form.parse(req,(err, fields, files) => {
-  //   if(err){
-  //     return res.status(404).json({status: false, error: "Error: Request is not parsed correctly"})
-  //   }
-  //   console.log("----------------------------",form._parser.globalOptions.maxFileSize)
-  //   const data = fs.readFileSync(files.file.filepath);
-  //   // comment for removing fs functionalities
-  //   // if (!fs.existsSync(resourcesDir)){
-  //   //   fs.mkdirSync(resourcesDir);
-  //   // }
-  //   // fs.writeFileSync(`${resourcesDir}/${files.file.originalFilename}`, data);
-  //   // if(!fs.existsSync(`${resourcesDir}/${files.file.originalFilename}`) && fs.existsSync(files.file.filepath))
-  //   // {
-  //   //   fs.unlinkSync(files.file.filepath);
-  //   //   console.log("temporery stored filr is deleted!")
-  //   //         return res.status(404).json({status: false, error: "error occure in file creation face"})
-  //   // }
-  //    res.status(200).json({status: true, path: files.file.filepath });
-  // });
 
 };
