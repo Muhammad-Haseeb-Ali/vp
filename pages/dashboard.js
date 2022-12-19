411fcd7d94756dad8e5c37d7844b95530ae1a161
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-12-19 22:26:23 +0500

    id

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -11,8 +11,8 @@ export default function PrivatePage(props) {
     event.preventDefault();
     const form = document.getElementById("proposalForm");
     const body = new FormData(form);
-    console.error([...body.entries()],body.entries()[1][1]);
-    fetch("https://backofvp.up.railway.app/proposal/" + body.entries()[1][1], {
+    console.error([...body.entries()]);
+    fetch("https://backofvp.up.railway.app/proposal/" + body.get('id'), {
       method: "POST",
       mode: 'no-cors',
       body
