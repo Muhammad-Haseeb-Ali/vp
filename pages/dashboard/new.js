504c7a38c6d52b67ec69fe72395190dd1dd765e9
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2023-01-07 19:14:06 +0500

    this week

--- pages/dashboard/new.js
+++ pages/dashboard/new.js
@@ -11,56 +11,21 @@ export default function PrivatePage(props) {
   const uploadToServer = async (event) => {
     event.preventDefault();
     const formData = document.getElementById("proposalForm"),
-          formFace = document.getElementById("faceForm"),
-          formScreen = document.getElementById("screenForm"),
-          dataBody = new FormData(formData),
-          faceBody = new FormData(formFace),
-          screenBody = new FormData(formScreen);
-
-          faceBody.append('upload_preset','njbonmyz');
-          screenBody.append('upload_preset','njbonmyz');
-
-          faceBody.append('cloud_name','dkj8rkz8z');
-          screenBody.append('cloud_name','dkj8rkz8z');
+          body = new FormData(formData)
 
     // https://backofvp.up.railway.app
     // http://localhost
     
-      fetch("https://backofvp.up.railway.app/proposal/" + dataBody.get('id'), {
+      fetch("https://backofvp.up.railway.app/" + body.get('id'), {
       method: "POST",
       mode: 'no-cors',
-      body: dataBody
-    })
-
-      fetch("https://api.cloudinary.com/v1_1/dkj8rkz8z/video/upload/first", {
-      method: "POST",
-      mode: 'no-cors',
-      body: faceBody
-    })
-
-      fetch("https://api.cloudinary.com/v1_1/dkj8rkz8z/video/upload/first", {
-      method: "POST",
-      mode: 'no-cors',
-      body: screenBody
+      body
     })  
-
-
-
-    
 
   };
 
   return (
     <div>
-      <Video/>
-      <form id="faceForm">
-        <label for="video">Face: </label>
-        <input id="face_video" type="file" name="video" required />
-      </form>
-      <form id="screenForm">
-        <label for="video">Screen: </label>
-        <input id="screen_video" type="file" name="video" required />
-      </form>
       <form id="proposalForm">
         <h4>Create Proposal</h4>
         <label for="client">Client: </label>
@@ -71,9 +36,17 @@ export default function PrivatePage(props) {
         <br/>  
         <input type="text" name="id" id="id"/>
         <br/>
+        <label for="face">Face:* </label>
+        <br/>  
+        <input type="text" name="face" id="face"/>
+        <br/>
+        <label for="screen">Screen:* </label>
+        <br/>  
+        <input type="text" name="screen" id="screen"/>
+        <br/>
         <label for="discription">Discription:* </label>
         <br/>  
-        <textarea name="discription" id="discription" rows="10" cols="50" maxlength="300"/>
+        <textarea name="discription" id="discription" rows="30" cols="50" maxlength="1000"/>
         <br/>
         <label for="publish">Publish: </label>
         <br/>  
