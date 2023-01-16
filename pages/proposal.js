2216b0ff4e89338719b15500e4d4684566d14d89
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2023-01-16 17:33:15 +0500

    error fixed

--- pages/proposal.js
+++ pages/proposal.js
@@ -46,7 +46,7 @@ export default function Proposal(props) {
             <title>For {MD.proposal.client !== "" ? MD.proposal.client : "you"}</title>
           </Head>
           <Navbar />
-          <StreamVideo videoLink={MD.proposal.screenLink} />
+          <StreamVideo videoLink={MD.proposal.videoLink} />
           <Reaction client={MD.proposal.client} />
           <Profile/>
           <Description client={MD.proposal.client || ""} discription={MD.proposal.discription} />
